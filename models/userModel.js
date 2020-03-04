ef703243a755fb550ab76b954eb31f7893c26c12
Harshal <abnaveharshal@gmail.com>
2020-03-04 21:03:11 +0530

    Hash Password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,8 +23,27 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please Confirm the password'],
-    minlength: 8
+    minlength: 8,
+    validate: {
+      // only work on SAVE and Create
+      validator: function(el) {
+        return el === this.password; // check if passwordConfirm with the password to see both are same or not
+      },
+      message: 'Password are not same'
+    }
   }
+});
+
+userSchema.pre('save', async function(next) {
+  // only run it the password is modified
+  if (!this.isModified('password')) return next();
+
+  //bootforce attack
+  //hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
